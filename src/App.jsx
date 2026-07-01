51a9f516ed1815e206c4872b6afc3848d9664873
saloqbi <saloqbi@gmail.com>
2026-07-01 13:00:17 +0300

    revert: remove forced 123 zoom patch

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,6 @@ import GannzillaWheelLineDrawPatch from './components/GannTools/GannzillaWheelLi
 import GannzillaArabicAiWheelSystemV1 from './components/GannTools/GannzillaArabicAiWheelSystemV1';
 import GannzillaDigitalNumberClarityPatch from './components/GannTools/GannzillaDigitalNumberClarityPatch';
 import GannzillaLongNumberDigitalRenderer from './components/GannTools/GannzillaLongNumberDigitalRenderer';
-import GannzillaDefaultZoom123Patch from './components/GannTools/GannzillaDefaultZoom123Patch';
 
 const App = () => {
   // للتبديل بين الصفحة الرئيسية وصفحة الاختبار
@@ -60,7 +59,6 @@ const App = () => {
               <GannzillaWheelPanButtonsPatch />
               <GannzillaCardinalBalancePatch />
               <GannzillaLongNumberDigitalRenderer />
-              <GannzillaDefaultZoom123Patch />
               <GannzillaOptionsBadgeHidePatch />
               <GannzillaPanelToggleTopbarPatch />
               <GannzillaWheelLineDrawPatch />
