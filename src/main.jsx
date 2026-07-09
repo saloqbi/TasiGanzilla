f0752506d995b67b934c5d26077045ae073cde85
saloqbi <saloqbi@gmail.com>
2026-07-10 02:47:31 +0300

    Remove duplicate drawing toggle overlay

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import GannzillaUnifiedDrawingPalettesV119 from './components/GannTools/GannzillaUnifiedDrawingPalettesV119';
 import './styles/globals.css';
-
-const isGannzillaProWheelMode = window.location.search.includes('gannzillaPro=true')
-  || window.location.search.includes('wheelPro=true');
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    {isGannzillaProWheelMode && <GannzillaUnifiedDrawingPalettesV119 />}
   </React.StrictMode>
 );
