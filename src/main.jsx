0c385b692844ff1d856d6c5ae02b56f254082e07
saloqbi <saloqbi@gmail.com>
2026-07-11 00:49:21 +0300

    Build 203 remove drawing-control overlay interference

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import GannzillaDrawingControlsLayoutV199 from './components/GannTools/GannzillaDrawingControlsLayoutV199';
 import './styles/globals.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <GannzillaDrawingControlsLayoutV199 />
   </React.StrictMode>
 );
