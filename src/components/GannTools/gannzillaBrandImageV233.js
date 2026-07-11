fccf5d493fa48c5829dadedae3590945e58fe98c
saloqbi <saloqbi@gmail.com>
2026-07-11 16:53:12 +0300

    Build 233 assemble HD About logo

---
+++ src/components/GannTools/gannzillaBrandImageV233.js
@@ -0,0 +1,6 @@
+import part1 from './brandV233/part1';
+import part2 from './brandV233/part2';
+import part3 from './brandV233/part3';
+import part4 from './brandV233/part4';
+
+export default `data:image/webp;base64,${part1}${part2}${part3}${part4}`;
