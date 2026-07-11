c4ba66fbdacdb3e6176950c62799b2654148cedf
saloqbi <saloqbi@gmail.com>
2026-07-11 16:30:13 +0300

    Build 232 publish right inset marker

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import TestPage from './pages/TestPage';
 import GannzillaArabicAiWheelSystemV1 from './components/GannTools/GannzillaArabicAiWheelSystemV1';
 import GannzillaBareWheelV224 from './components/GannTools/GannzillaBareWheelV224';
 
-// Build 231: stable V228 wheel plus the single-owner top toolbar.
+// Build 232: About icon aligned right with a 38px toolbar inset.
 const App = () => {
   const search = window.location.search;
   const isTestMode = search.includes('test=true');
@@ -18,7 +18,7 @@ const App = () => {
   return (
     <ToolProvider>
       <LanguageProvider>
-        <div data-gannzilla-build="231">
+        <div data-gannzilla-build="232">
           {isArabicAiWheelMode ? (
             <GannzillaArabicAiWheelSystemV1 />
           ) : isGannzillaProWheelMode ? (
