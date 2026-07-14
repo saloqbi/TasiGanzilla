4168a4312f4a0594c9e1c04a0d764f7fd10abbcd
saloqbi <saloqbi@gmail.com>
2026-07-15 00:58:39 +0300

    Make v391 preview open directly to restored wheel

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,19 @@
 import React from 'react';
 import { ToolProvider } from './context/ToolContext';
 import { LanguageProvider } from './context/LanguageContext';
-import Home from './pages/Home';
-import HomeEnhanced from './pages/HomeEnhanced';
-import TestPage from './pages/TestPage';
-import GannzillaArabicAiWheelSystemV1 from './components/GannTools/GannzillaArabicAiWheelSystemV1';
 import GannzillaBareWheelV224 from './components/GannTools/GannzillaBareWheelV224';
 import GannzillaMovableRedGreenProtractorFrameV390 from './components/GannTools/GannzillaMovableRedGreenProtractorFrameV390';
 
-// Build 391: protect angle labels by splitting red ticks and adding a clean white halo.
-const App = () => {
-  const search = window.location.search;
-  const isTestMode = search.includes('test=true');
-  const isArabicAiWheelMode = search.includes('gannzillaArabicAI=true') || search.includes('aiWheel=true');
-  const isGannzillaProWheelMode = search.includes('gannzillaPro=true') || search.includes('wheelPro=true');
-  const isEnhancedMode = search.includes('enhanced=true') || true;
-
-  return (
-    <ToolProvider>
-      <LanguageProvider>
-        <div data-gannzilla-build="391">
-          {isArabicAiWheelMode ? (
-            <GannzillaArabicAiWheelSystemV1 />
-          ) : isGannzillaProWheelMode ? (
-            <>
-              <GannzillaBareWheelV224 />
-              <GannzillaMovableRedGreenProtractorFrameV390 />
-            </>
-          ) : isTestMode ? (
-            <TestPage />
-          ) : isEnhancedMode ? (
-            <HomeEnhanced />
-          ) : (
-            <Home />
-          )}
-        </div>
-      </LanguageProvider>
-    </ToolProvider>
-  );
-};
+// Exact v391 preview: open directly to the restored Gannzilla wheel.
+const App = () => (
+  <ToolProvider>
+    <LanguageProvider>
+      <div data-gannzilla-build="391">
+        <GannzillaBareWheelV224 />
+        <GannzillaMovableRedGreenProtractorFrameV390 />
+      </div>
+    </LanguageProvider>
+  </ToolProvider>
+);
 
 export default App;
