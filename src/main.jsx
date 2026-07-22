2d04b34bc7216d70ba98f9cfc23d5345dd2bd237
saloqbi <saloqbi@gmail.com>
2026-07-23 02:42:02 +0300

    Build 392: activate clean draggable wheel viewport

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './components/GannTools/gannzillaBrightWheelLinesV390';
+import './components/GannTools/gannzillaCleanDragViewportV392';
 import './styles/globals.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
