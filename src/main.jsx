b82c5d9ff6ccbc39e141a663534054babd348ba3
saloqbi <saloqbi@gmail.com>
2026-07-23 20:49:02 +0300

    feat: enable readable wide LTR reference panel v422

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,7 @@ import './components/GannTools/gannzillaRepeatFirstRingOneToNineV415';
 import './components/GannTools/gannzillaInnerStartRingOneToThirtySixV416';
 import './components/GannTools/gannzillaKeyboardMouseControlV413';
 import './components/GannTools/gannzillaHideNativeWheelScrollbarsV417';
+import './components/GannTools/gannzillaReferencePanelLTRWideV422';
 import './styles/globals.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
