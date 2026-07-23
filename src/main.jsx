b177ba5083ae83f74a3543ce5dcedfd9e032ab8c
saloqbi <saloqbi@gmail.com>
2026-07-23 03:05:30 +0300

    Build 396: load toolbar deduplication guard

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App';
 import './components/GannTools/gannzillaBrightWheelLinesV390';
 import './components/GannTools/gannzillaCleanDragViewportV392';
 import './components/GannTools/gannzillaToolbarRestoreV394';
+import './components/GannTools/gannzillaToolbarDeduplicateV396';
 import './styles/globals.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
