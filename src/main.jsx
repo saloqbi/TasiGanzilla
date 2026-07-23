66139f2326a8a8fe5cfd93ab3cd088953b759d7a
saloqbi <saloqbi@gmail.com>
2026-07-24 00:29:10 +0300

    fix: load robust language control v435

--- src/main.jsx
+++ src/main.jsx
@@ -15,6 +15,7 @@ import './components/GannTools/gannzillaEnlargeCopyIconV425';
 import './components/GannTools/gannzillaPngWindowsAssociationV428';
 import './components/GannTools/gannzillaAboutLogoV432';
 import './components/GannTools/gannzillaBilingualPanelSelectorV434';
+import './components/GannTools/gannzillaLanguageControlV435';
 import './styles/globals.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
