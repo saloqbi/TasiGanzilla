0c2cb937f5ca323d35d7f56028bd2d77f32379b9
saloqbi <saloqbi@gmail.com>
2026-07-23 15:29:05 +0300

    Build 406: load permanent clipping for the two outer base-wheel frames

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App';
 import './components/GannTools/gannzillaBrightWheelLinesV390';
 import './components/GannTools/gannzillaCleanDragViewportV392';
 import './components/GannTools/gannzillaRemoveSideShapeBarV401';
+import './components/GannTools/gannzillaClipOuterFramesV406';
 import './styles/globals.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
