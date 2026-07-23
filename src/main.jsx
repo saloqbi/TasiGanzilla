d816d4d42b4ae3af0aeb81b3c3134672f278a2f6
saloqbi <saloqbi@gmail.com>
2026-07-23 22:51:18 +0300

    fix: load protected top reference toolbar v430

--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,7 @@ import './components/GannTools/gannzillaReferencePanelExtraLargeV423';
 import './components/GannTools/gannzillaRemovePanelBrandV424';
 import './components/GannTools/gannzillaEnlargeCopyIconV425';
 import './components/GannTools/gannzillaPngWindowsAssociationV428';
-import './components/GannTools/gannzillaTopReferenceToolbarV429';
+import './components/GannTools/gannzillaTopReferenceToolbarV430';
 import './styles/globals.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
