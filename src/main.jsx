258c642bab496b49ae91d23e72e4cf294f306122
saloqbi <saloqbi@gmail.com>
2026-07-23 15:09:00 +0300

    Build 404: stop loading legacy single-toolbar layout patch

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './components/GannTools/gannzillaBrightWheelLinesV390';
 import './components/GannTools/gannzillaCleanDragViewportV392';
-import './components/GannTools/gannzillaSingleToolbarLayoutV399';
 import './components/GannTools/gannzillaRemoveSideShapeBarV401';
 import './styles/globals.css';
 
