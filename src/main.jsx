c289022dae75567c05cf6666ca46f9ca1f854f45
saloqbi <saloqbi@gmail.com>
2026-07-24 02:35:50 +0300

    feat: load wheel movement control v444

--- src/main.jsx
+++ src/main.jsx
@@ -18,6 +18,7 @@ import './components/GannTools/gannzillaBilingualPanelSelectorV434';
 import './components/GannTools/gannzillaRightLanguageControlV438';
 import './components/GannTools/gannzillaConnectionSettingsV439';
 import './components/GannTools/gannzillaZoomFullscreenStripV443';
+import './components/GannTools/gannzillaWheelMoveControlV444';
 import './styles/globals.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
