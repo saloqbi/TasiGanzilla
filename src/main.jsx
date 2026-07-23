4241726fe1bda0c59e19ff9469a5a90072790c76
saloqbi <saloqbi@gmail.com>
2026-07-23 15:57:56 +0300

    Build 408: load radial comfort fitting for wheel numbers

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,11 @@ import './components/GannTools/gannzillaBrightWheelLinesV390';
 import './components/GannTools/gannzillaCleanDragViewportV392';
 import './components/GannTools/gannzillaRemoveSideShapeBarV401';
 import './components/GannTools/gannzillaClipOuterFramesV406';
-import './components/GannTools/gannzillaComfortCellNumbersV407';
+import './components/GannTools/gannzillaComfortRadialCellNumbersV408';
 import './styles/globals.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
