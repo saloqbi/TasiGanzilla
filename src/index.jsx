71809f1b446e9c374f0191b2e4fc301b48df9d32
saloqbi <saloqbi@gmail.com>
2026-07-24 19:12:06 +0300

    Enable chart viewport offset below the drawing toolbar

---
+++ src/index.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './components/GannTools/gannzillaBrightWheelLinesV390';
+import './components/GannTools/gannzillaCleanDragViewportV392';
+import './components/GannTools/gannzillaRemoveSideShapeBarV401';
+import './components/GannTools/gannzillaUprightSymmetricAllNumbersV412';
+import './components/GannTools/gannzillaRepeatFirstRingOneToNineV415';
+import './components/GannTools/gannzillaInnerStartRingOneToThirtySixV416';
+import './components/GannTools/gannzillaKeyboardMouseControlV459';
+import './components/GannTools/gannzillaHideNativeWheelScrollbarsV417';
+import './components/GannTools/gannzillaReferencePanelExtraLargeV423';
+import './components/GannTools/gannzillaRemovePanelBrandV424';
+import './components/GannTools/gannzillaEnlargeCopyIconV425';
+import './components/GannTools/gannzillaPngWindowsAssociationV428';
+import './components/GannTools/gannzillaAboutLogoV432';
+import './components/GannTools/gannzillaBilingualPanelSelectorV434';
+import './components/GannTools/gannzillaRightLanguageControlV438';
+import './components/GannTools/gannzillaConnectionSettingsV439';
+import './components/GannTools/gannzillaUnifiedWheelToolsV453';
+import './components/GannTools/gannzillaPanelVisibilityEyeV457';
+import './components/GannTools/gannzillaShapesMenuV460';
+import './components/GannTools/gannzillaShapesMenuEnlargeV461';
+import './components/GannTools/gannzillaChartDrawingToolbarV466';
+import './components/GannTools/gannzillaChartDrawingViewportOffsetV467';
+import './styles/globals.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
