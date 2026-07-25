968bcc2f374476f9d1312eff8cb7b87c4333d8f6
saloqbi <saloqbi@gmail.com>
2026-07-25 18:30:27 +0300

    Load enlarged uniform wheel number typography v487

--- src/main.jsx
+++ src/main.jsx
@@ -32,6 +32,7 @@ import './components/GannTools/gannzillaUnlimitedRingThemeBridgeV481';
 import './components/GannTools/gannzillaAllToolsRuntimeV482';
 import './components/GannTools/gannzillaClockwiseDirectionBridgeV483';
 import './components/GannTools/gannzillaCanonicalWheelGeometryV486';
+import './components/GannTools/gannzillaNumberClarityV487';
 import './styles/globals.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
