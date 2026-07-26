61c79353cdd9cad3ef0d2a72336d923856916c63
saloqbi <saloqbi@gmail.com>
2026-07-26 07:35:18 +0300

    Bind paint preview zoom controls v513

--- src/main.jsx
+++ src/main.jsx
@@ -23,6 +23,7 @@ import './components/GannTools/gannzillaFinalWheelAuthorityV506';
 import './components/GannTools/gannzillaCenterCellComfortV508';
 import './components/GannTools/gannzillaStablePanWheelV510';
 import './components/GannTools/gannzillaFixedPaintPreviewV512';
+import './components/GannTools/gannzillaPaintZoomControlsV513';
 import './styles/globals.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
