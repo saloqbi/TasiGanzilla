c998ebcaeff3ac013a42ca8b0ef5737c41a6f18b
saloqbi <saloqbi@gmail.com>
2026-07-26 07:26:55 +0300

    Enable fixed paint preview v512

--- src/main.jsx
+++ src/main.jsx
@@ -22,6 +22,7 @@ import './components/GannTools/gannzillaClockwiseDirectionBridgeV483';
 import './components/GannTools/gannzillaFinalWheelAuthorityV506';
 import './components/GannTools/gannzillaCenterCellComfortV508';
 import './components/GannTools/gannzillaStablePanWheelV510';
+import './components/GannTools/gannzillaFixedPaintPreviewV512';
 import './styles/globals.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
