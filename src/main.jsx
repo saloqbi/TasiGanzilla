06dbe198faa84b4aab532fa79ced4d9ccd9d5dfa
saloqbi <saloqbi@gmail.com>
2026-07-26 08:14:00 +0300

    Revert to approved paint zoom authority v514

--- src/main.jsx
+++ src/main.jsx
@@ -22,7 +22,8 @@ import './components/GannTools/gannzillaClockwiseDirectionBridgeV483';
 import './components/GannTools/gannzillaFinalWheelAuthorityV506';
 import './components/GannTools/gannzillaCenterCellComfortV508';
 import './components/GannTools/gannzillaStablePanWheelV510';
-import './components/GannTools/gannzillaUnifiedPaintPreviewZoomV515';
+import './components/GannTools/gannzillaFixedPaintPreviewV512';
+import './components/GannTools/gannzillaPaintZoomAuthorityV514';
 import './styles/globals.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
