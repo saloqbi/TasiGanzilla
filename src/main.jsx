dace17be1bb0a5e85f578e9d50524cf9bd574197
saloqbi <saloqbi@gmail.com>
2026-07-28 12:37:39 +0300

    Activate final right nudge V570

--- src/main.jsx
+++ src/main.jsx
@@ -56,6 +56,7 @@ import './components/GannTools/gannzillaTasiTimeTrackerMainTimeBalanceV566';
 import './components/GannTools/gannzillaTasiTimeTrackerMainTimeCenterV567';
 import './components/GannTools/gannzillaTasiTimeTrackerResponsiveFitV568';
 import './components/GannTools/gannzillaTasiTimeTrackerMainTimeRightCenterV569';
+import './components/GannTools/gannzillaTasiTimeTrackerFinalRightV570';
 import './styles/globals.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
