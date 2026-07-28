31aa5be4a6ad6f128e23f210c1f2824602a562cb
saloqbi <saloqbi@gmail.com>
2026-07-28 13:17:39 +0300

    Activate tracker interaction isolation V572

--- src/main.jsx
+++ src/main.jsx
@@ -58,6 +58,7 @@ import './components/GannTools/gannzillaTasiTimeTrackerResponsiveFitV568';
 import './components/GannTools/gannzillaTasiTimeTrackerMainTimeRightCenterV569';
 import './components/GannTools/gannzillaTasiTimeTrackerFinalRightV570';
 import './components/GannTools/gannzillaTasiTimeTrackerManualFrameV571';
+import './components/GannTools/gannzillaTasiTimeTrackerInteractionFixV572';
 import './styles/globals.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
