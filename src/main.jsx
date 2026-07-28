31a9f7e13773a3ae1d00c7dd4ff9faabae12797e
saloqbi <saloqbi@gmail.com>
2026-07-28 17:13:37 +0300

    Load V578 time tracker toolbar toggle

--- src/main.jsx
+++ src/main.jsx
@@ -59,6 +59,7 @@ import './components/GannTools/gannzillaTasiTimeTrackerMainTimeRightCenterV569';
 import './components/GannTools/gannzillaTasiTimeTrackerFinalRightV570';
 import './components/GannTools/gannzillaTasiTimeTrackerFourEdgeResizeV576';
 import './components/GannTools/gannzillaTasiTimeTrackerMainTimeSafeFitV577';
+import './components/GannTools/gannzillaTasiTimeTrackerToolbarToggleV578';
 import './styles/globals.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
