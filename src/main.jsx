84023b4871ac649a7d8efc3d8d95436acbecba79
saloqbi <saloqbi@gmail.com>
2026-07-28 08:12:22 +0300

    Activate larger time tracker rows V553

--- src/main.jsx
+++ src/main.jsx
@@ -40,6 +40,7 @@ import './components/GannTools/gannzillaTasiTimeTrackerLuxuryStyleV549';
 import './components/GannTools/gannzillaTasiTimeTrackerClarityV550';
 import './components/GannTools/gannzillaTasiTimeTrackerOrderClarityV551';
 import './components/GannTools/gannzillaTasiTimeTrackerTextClarityV552';
+import './components/GannTools/gannzillaTasiTimeTrackerRowSizeV553';
 import './styles/globals.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
