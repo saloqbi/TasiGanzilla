5823c6584b5a0e7ae0e35245924ce1e31c02b520
saloqbi <saloqbi@gmail.com>
2026-07-28 08:48:57 +0300

    Activate time tracker frame glow V555

--- src/main.jsx
+++ src/main.jsx
@@ -42,6 +42,7 @@ import './components/GannTools/gannzillaTasiTimeTrackerOrderClarityV551';
 import './components/GannTools/gannzillaTasiTimeTrackerTextClarityV552';
 import './components/GannTools/gannzillaTasiTimeTrackerRowSizeV553';
 import './components/GannTools/gannzillaTasiTimeTrackerFullWidthRowsV554';
+import './components/GannTools/gannzillaTasiTimeTrackerFrameGlowV555';
 import './styles/globals.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
