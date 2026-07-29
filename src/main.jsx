255495e7b59267a92422278570128e46b15b2d0d
saloqbi <saloqbi@gmail.com>
2026-07-29 03:28:05 +0300

    Replace legacy center logo overlays with static canvas logo V582

--- src/main.jsx
+++ src/main.jsx
@@ -60,9 +60,7 @@ import './components/GannTools/gannzillaTasiTimeTrackerFinalRightV570';
 import './components/GannTools/gannzillaTasiTimeTrackerFourEdgeResizeV576';
 import './components/GannTools/gannzillaTasiTimeTrackerMainTimeSafeFitV577';
 import './components/GannTools/gannzillaTasiTimeTrackerToolbarToggleV578';
-import './components/GannTools/gannzillaCenterLogoV579';
-import './components/GannTools/gannzillaCenterLogoLayerFixV580';
-import './components/GannTools/gannzillaCenterLogoVisibleCanvasV581';
+import './components/GannTools/gannzillaCenterLogoStaticCanvasV582';
 import './styles/globals.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
