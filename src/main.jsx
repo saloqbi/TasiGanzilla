c78b9b2cf6e92e9396ca5e2b85b194d8bcd82cd4
saloqbi <saloqbi@gmail.com>
2026-07-29 04:46:14 +0300

    Load V586 center logo retry authority

--- src/main.jsx
+++ src/main.jsx
@@ -61,6 +61,7 @@ import './components/GannTools/gannzillaTasiTimeTrackerFourEdgeResizeV576';
 import './components/GannTools/gannzillaTasiTimeTrackerMainTimeSafeFitV577';
 import './components/GannTools/gannzillaTasiTimeTrackerToolbarToggleV578';
 import './components/GannTools/gannzillaCenterLogoRealJpegV585';
+import './components/GannTools/gannzillaCenterLogoRetryV586';
 import './styles/globals.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
