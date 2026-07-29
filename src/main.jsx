8a91b91b0ef6a920b0734f45dc5652d994be1565
saloqbi <saloqbi@gmail.com>
2026-07-29 10:15:35 +0300

    Use single center logo 81 authority V596

--- src/main.jsx
+++ src/main.jsx
@@ -60,8 +60,7 @@ import './components/GannTools/gannzillaTasiTimeTrackerFinalRightV570';
 import './components/GannTools/gannzillaTasiTimeTrackerFourEdgeResizeV576';
 import './components/GannTools/gannzillaTasiTimeTrackerMainTimeSafeFitV577';
 import './components/GannTools/gannzillaTasiTimeTrackerToolbarToggleV578';
-import './components/GannTools/gannzillaCenterLogoResponsive78V594';
-import './components/GannTools/gannzillaCenterLogoResponsive81V595';
+import './components/GannTools/gannzillaCenterLogoResponsive81V596';
 import './styles/globals.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
