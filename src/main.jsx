6cf290619a40a6c75dbcb873c947cef674cfe7f0
saloqbi <saloqbi@gmail.com>
2026-07-30 14:12:15 +0300

    Activate V651 one-time force top dock after V650

--- src/main.jsx
+++ src/main.jsx
@@ -40,6 +40,7 @@ import './components/GannTools/gannzillaCompactIconTrialV640';
 import './components/GannTools/gannzillaTopIconShadowRemovalV641';
 import './components/GannTools/gannzillaThinTopBoundaryV642';
 import './components/GannTools/gannzillaWheelTopBandOwnerFixV650';
+import './components/GannTools/gannzillaForceWheelTopDockV651';
 import './components/GannTools/gannzillaTasiTimeTrackerV547';
 import './components/GannTools/gannzillaTasiTimeTrackerGridFixV548';
 import './components/GannTools/gannzillaTasiTimeTrackerLuxuryStyleV549';
