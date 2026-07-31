7066c0fff9ea1cb52b554a3a44dc4974053b96cd
saloqbi <saloqbi@gmail.com>
2026-07-31 20:50:07 +0300

    Load ornate frame overlay after wheel authorities

--- src/main.jsx
+++ src/main.jsx
@@ -87,6 +87,7 @@ import './components/GannTools/gannzillaPanelExactMirrorSilverV669';
 import './components/GannTools/gannzillaToolbarFullMirrorStripV672';
 import './components/GannTools/gannzillaToolbarIconFramesRestoreV673';
 import './components/GannTools/gannzillaWheelIvoryChampagneFinalAuthorityV682';
+import './components/GannTools/gannzillaOrnateOuterFrameOverlayV687';
 import './styles/globals.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
