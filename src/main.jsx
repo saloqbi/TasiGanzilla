5d4cc4ecf9ba130b514b5f2d35f0ccedecbb0e5f
saloqbi <saloqbi@gmail.com>
2026-07-31 12:58:33 +0300

    Activate short-link geometry bridge before champagne overlay

--- src/main.jsx
+++ src/main.jsx
@@ -84,6 +84,7 @@ import './components/GannTools/gannzillaOuterEmptyRingMirrorSilverV668';
 import './components/GannTools/gannzillaPanelExactMirrorSilverV669';
 import './components/GannTools/gannzillaToolbarFullMirrorStripV672';
 import './components/GannTools/gannzillaToolbarIconFramesRestoreV673';
+import './components/GannTools/gannzillaShortLinkWheelGeometryBridgeV679';
 import './components/GannTools/gannzillaWheelChampagneChromeOverlayV675';
 import './components/GannTools/gannzillaReferenceGlossShineV676';
 import './styles/globals.css';
